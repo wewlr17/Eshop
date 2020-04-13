17a1265a50995335fc53dc98f1b4b0b523c0df75
dgabin <depairegabin@MacBook-Pro.local>
2020-04-13 23:23:56 +0200

    UPD menu

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -115,8 +115,6 @@
                     </li>
                         <li class="nav-item dropdown">
                     
-                           <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>  Menu <span class="caret"></span>
-                             </a> 
                             </li>
                         
                             <li class="nav-item dropdown">
